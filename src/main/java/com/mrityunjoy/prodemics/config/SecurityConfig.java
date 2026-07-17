0392722b119d65e5fa3b2a36e8abaf572e9a14f4
Mrityunjoy Nath <mrityunjoynath38@gmail.com>
2026-07-17 15:21:15 +0530

    Upgraded to JJWT 0.13.0

--- src/main/java/com/mrityunjoy/prodemics/config/SecurityConfig.java
+++ src/main/java/com/mrityunjoy/prodemics/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,7 +19,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
