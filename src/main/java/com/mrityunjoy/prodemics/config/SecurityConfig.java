f72b19ebfa7b193b05211e4689a790f7034209ba
Mrityunjoy Nath <mrityunjoynath38@gmail.com>
2026-07-17 15:51:47 +0530

    Moved from JJWT to NimbusJwtEncoder as Spring resource server already has the jose dependency

--- src/main/java/com/mrityunjoy/prodemics/config/SecurityConfig.java
+++ src/main/java/com/mrityunjoy/prodemics/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.mrityunjoy.prodemics.config;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
-import io.jsonwebtoken.security.Keys;
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,14 +16,18 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 public class SecurityConfig {
@@ -72,10 +76,18 @@ public class SecurityConfig {
 	}
 
 	@Bean
+    JwtEncoder jwtEncoder() {
+		final String SECRET = "abcdreetretesdfesresdtrgfdtrdjyfdytftyyfytjftyf";
+		SecretKey key = new SecretKeySpec(SECRET.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+
+		return new NimbusJwtEncoder(new ImmutableSecret<>(key));
+	}
+
+	@Bean
     JwtDecoder jwtDecoder() {
 		final String SECRET = "abcdreetretesdfesresdtrgfdtrdjyfdytftyyfytjftyf";
-		SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+		SecretKey key = new SecretKeySpec(SECRET.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
 
-		return NimbusJwtDecoder.withSecretKey(key).build();
+		return NimbusJwtDecoder.withSecretKey(key).macAlgorithm(MacAlgorithm.HS256).build();
 	}
 }
--- src/main/java/com/mrityunjoy/prodemics/service/JwtService.java
+++ src/main/java/com/mrityunjoy/prodemics/service/JwtService.java
@@ -1,40 +1,37 @@
 package com.mrityunjoy.prodemics.service;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtService {
+    @Autowired
+    private JwtEncoder jwtEncoder;
+
     public String generateToken(
-            Authentication auth) {
+            Authentication authentication) {
 
-        if (auth == null) return null;
+        Instant now = Instant.now();
 
-        SecretKey key = Keys
-                .hmacShaKeyFor("abcdreetretesdfesresdtrgfdtrdjyfdytftyyfytjftyf".getBytes(StandardCharsets.UTF_8));
-        String jwt = Jwts.builder().setIssuer("prodemics").setSubject("JWT Token")
-                .claim("username", auth.getName())
-                .claim("scope", populateAuthorities(auth.getAuthorities())).setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime() + (3600 * 1000))).signWith(key).compact();
+        String scope = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
 
-        return jwt;
-    }
+        JwtClaimsSet claims = JwtClaimsSet.builder().issuer("prodemics").subject(authentication.getName())
+                .issuedAt(now).expiresAt(now.plus(1, ChronoUnit.HOURS)).claim("scope", scope)
+                .build();
+        JwsHeader header = JwsHeader.with(MacAlgorithm.HS256).build();
 
-    private String populateAuthorities(Collection<? extends GrantedAuthority> collection) {
-        Set<String> authoritiesSet = new HashSet<>();
-        for (GrantedAuthority authority : collection) {
-            authoritiesSet.add(authority.getAuthority());
-        }
-        return String.join(" ", authoritiesSet);
+        return jwtEncoder.encode(JwtEncoderParameters.from(header, claims)).getTokenValue();
     }
 }
