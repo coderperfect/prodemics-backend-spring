baeb8f88af05fe1b47ffb94dc790d1b0031dd15b
Mrityunjoy Nath <mrityunjoynath38@gmail.com>
2026-07-20 10:49:50 +0530

    Added end user service and removed the manual implementation of basic authentication manager

---
+++ src/main/java/com/mrityunjoy/prodemics/service/EndUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.mrityunjoy.prodemics.service;
+
+import com.mrityunjoy.prodemics.model.EndUser;
+import com.mrityunjoy.prodemics.model.Role;
+import com.mrityunjoy.prodemics.repository.EndUserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EndUserDetailsService implements UserDetailsService {
+
+    private final EndUserRepository repository;
+
+    public EndUserDetailsService(EndUserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    @NonNull
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        EndUser user = repository.getByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        String[] authorities = user.getRoles().stream().map(Role::getName).toArray(String[]::new);
+
+        return User.builder().username(user.getUsername()).password(user.getPassword()).authorities(authorities)
+                .build();
+    }
+}
